ca8311260b4862ea3caa669dc80c4895ff231eb8
joongwon <anjoongwon517@gmail.com>
2025-08-03 16:34:41 +0900

    feat: 1장 연습문제 구현

---
+++ src/main/java/com/sprint/jw/godofjava/Volume1/Chapter01/Calculator.java
@@ -0,0 +1,29 @@
+package com.sprint.jw.godofjava.Volume1.Chapter01;
+
+public class Calculator {
+
+  public int add(int a, int b) {
+    int result = a + b;
+    return result;
+  }
+
+  public int addAll(int a, int b, int c) {
+    int result = a + b + c;
+    return result;
+  }
+
+  public int subtract(int a, int b) {
+    int result = a - b;
+    return result;
+  }
+
+  public int multiply(int a, int b) {
+    int result = a * b;
+    return result;
+  }
+
+  public int divide(int a, int b) {
+    int result = a / b;
+    return result;
+  }
+}
