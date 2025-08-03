773cd64cfdda835d6f7331ccf2bb06b4f0cad4fd
joongwon <anjoongwon517@gmail.com>
2025-08-03 16:34:11 +0900

    chore: 기초 세팅

---
+++ src/main/java/com/sprint/jw/godofjava/GodOfJavaApplication.java
@@ -0,0 +1,13 @@
+package com.sprint.jw.godofjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodOfJavaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GodOfJavaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sprint/jw/godofjava/GodOfJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.jw.godofjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodOfJavaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
