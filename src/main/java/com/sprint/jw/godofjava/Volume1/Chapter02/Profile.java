da843e58b94354813fe1f1e1a14ca629a90e9b72
joongwon <anjoongwon517@gmail.com>
2025-08-10 21:22:30 +0900

    feat: 2장 연습문제 구현

---
+++ src/main/java/com/sprint/jw/godofjava/Volume1/Chapter02/Profile.java
@@ -0,0 +1,9 @@
+package com.sprint.jw.godofjava.Volume1.Chapter02;
+
+public class Profile {
+
+    public static void main(String[] args) {
+        System.out.println("My name is jw");
+        System.out.println("My age is 29");
+    }
+}
