da401142dd8eed9eb145137d3811e8234ab0d9c8
joongwon <anjoongwon517@gmail.com>
2025-08-28 22:11:24 +0900

    [5장] 연산자 - 비트 NOT 연산자

---
+++ src/main/java/com/sprint/jw/godofjava/Volume1/Chapter05/OperatorComplement.java
@@ -0,0 +1,14 @@
+package com.sprint.jw.godofjava.Volume1.Chapter05;
+
+public class OperatorComplement {
+    public static void main(String[] args) {
+        OperatorComplement operatorComplement = new OperatorComplement();
+        operatorComplement.complement();
+    }
+
+    public void complement() {
+        boolean flag = true;
+        System.out.println(flag);
+        System.out.println(!flag);
+    }
+}
